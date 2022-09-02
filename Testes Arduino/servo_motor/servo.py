8924b22cd1efd9f35b9e351b887e36ae7ccae8a4
Rodrigo Piva <rodrigobragatopiva@gmail.com>
2022-09-02 11:50:54 -0300

    Controle do Servo

--- Testes Arduino/servo_motor/servo.py
+++ Testes Arduino/servo_motor/servo.py
@@ -4,24 +4,34 @@ import time
 
 while True: #Loop para a conexão com o Arduino
     try:  #Tenta se conectar, se conseguir, o loop se encerra
-        arduino = serial.Serial('COM3', 9600, dsrdtr = None, timeout=3)
+        arduino = serial.Serial('COM5', 9600, dsrdtr = None, timeout=4)
         print('Arduino conectado')
         break
     except:
         pass
-time.sleep(5)
+time.sleep(4)
 
-arduino.flushInput()
-arduino.flushOutput()
+# arduino.flushInput()
+# arduino.flushOutput()
 
 time.sleep(.1)
 
-arduino.write(chr(180).encode())
-arduino.write(chr(45).encode())
+# arduino.write(chr(180).encode())
+# arduino.write(chr(45).encode())
+
+num1 = 270
+num2 = 30
+
+msg = num1.to_bytes(2, 'little')+num2.to_bytes(2, 'little')
+
+print(msg)
+
+arduino.write(msg)
 
 # while arduino.in_waiting:
 #     print (arduino.readline())
-for i in range(5):
+for i in range(10):
     txt=arduino.read()
-    print(txt)
-    print(int.from_bytes(txt, 'little'))
+    if(txt != b''):
+        print(txt)
+        print(int.from_bytes(txt, 'little'))
--- Testes Arduino/servo_motor/servo_motor.ino
+++ Testes Arduino/servo_motor/servo_motor.ino
@@ -6,6 +6,7 @@ int16_t msg[13];        //variavel que recebe todas as infos do python
 int16_t pos_at[7];      //variavel que guarda a posicao atual dos membros do robô
 int16_t incremento[5]; //variavel que guarda o quanto os servos têm que se movimentar
 int i, j, pos;
+byte buff[4];
 
 void setup() {
   Serial.begin(9600);
@@ -13,42 +14,52 @@ void setup() {
   memset(msg,0,13*sizeof(int16_t)); //set msg to 0
   memset(pos_at,0,7*sizeof(int16_t)); //set msg to 0
 
-  Serial.flush();
+  myservo.write(0);
 }
 
 void loop() {
   //Código para comunicação com python
   while(!Serial.available());
   for(i=0;i<2;i++){
-    msg[i] = Serial.read();
-    //Serial.readBytes(buffer, sizeof(int))
+//    msg[i] = Serial.read();
   }
-
-
+  Serial.readBytes(buff, 4);
+  msg[0] = buff[0] + (buff[1] << 8);
+  msg[1] = buff[2] + (buff[3] << 8);
+//  Serial.write(buff);
+  
   //Serial.readBytes((char*)msg, 13*sizeof(int16_t));
   //Serial.println(msg[0]);
 
-  //Cálculo para movimento do braço
   for(i=0; i<2; i++){
-    incremento[i] = (msg[i]-pos_at[i])/200;
+    delay(3000);
+    myservo.write(msg[i]);
   }
 
-  for(i=0;i<200; i++){
-    myservo.write(pos_at[0]);
-    delay(5);
-    pos_at[0]=pos_at[0]+incremento[0];
-  }
-
-  incremento[1] = (msg[1]-pos_at[0])/200;
   
-  for(i=0;i<200; i++){
-    myservo.write(pos_at[0]);
-    delay(5);
-    pos_at[0]=pos_at[0]+incremento[1];
-  }
+
+  //Cálculo para movimento do braço
+//  for(i=0; i<2; i++){
+//    incremento[i] = (msg[i]-pos_at[i])/200;
+//  }
+//
+//  for(i=0;i<200; i++){
+//    myservo.write(pos_at[0]);
+//    delay(15);
+//    pos_at[0]=pos_at[0]+incremento[0];
+//  }
+//
+//  incremento[1] = (msg[1]-pos_at[0])/200;
+//  
+//  for(i=0;i<200; i++){
+//    myservo.write(pos_at[0]);
+//    delay(15);
+//    pos_at[0]=pos_at[0]+incremento[1];
+//  }
   
   Serial.write(msg[0]);
-  Serial.write(msg[1]);
+  Serial.print(msg[0]);
+//  Serial.write(msg[1]);
   
 
   /*  for(i=0; i<5; i++){
